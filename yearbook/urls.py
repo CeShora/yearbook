5653f0269ba89a4a5ebfeb91390c8166c004fd9a
neginkheirmand <neginkheirmand@gmail.com>
2024-07-03 08:30:03 +0330

    add todo list

--- yearbook/urls.py
+++ yearbook/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('students/', views.student_list, name='student_list'),
-    path('student/new/', views.student_create, name='student_create'),  # Correctly define the URL pattern for creating a new student
-    path('student/<str:student_id>/', views.student_detail, name='student_detail'),
+    path('', views.home, name='home'), #TODO ADD HALL OF FAME, GALLERY, CLEAN A BIT
+    path('students/', views.student_list, name='student_list'), # NEED TO ADD FILTERS
+    path('student/new/', views.student_create, name='student_create'),  # JUST CHECK ABOUT LOGIN
+    path('student/<str:student_id>/', views.student_detail, name='student_detail'), # ADD THE IMAGE LATER
     path('student/<str:student_id>/edit/', views.student_update, name='student_update'),
-    path('student/<str:student_id>/delete/', views.student_delete, name='student_delete'),
+    # path('student/<str:student_id>/delete/', views.student_delete, name='student_delete'),
 ]
 
 
