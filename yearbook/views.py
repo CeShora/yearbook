c234edfff8ebc845c2b9c34307dacec0beef87d6
neginkheirmand <neginkheirmand@gmail.com>
2024-07-03 06:26:02 +0330

    updated views

--- yearbook/views.py
+++ yearbook/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import QUESTIONS, Student
-from .forms import StudentProfileForm, AnswerFormSet
+from .models import QUESTIONS, Answer, Student
+from .forms import StudentProfileForm, AnswerFormSet, get_dynamic_answer_forms
 from .forms import StudentRegistrationForm
 
 def home(request):
@@ -17,18 +17,33 @@ def student_detail(request, student_id):
     answers = student.answers.all()  # Fetch all related answers for the student
     return render(request, 'yearbook/student_detail.html', {'student': student, 'answers': answers, 'questions': QUESTIONS})
 
-
 def student_create(request):
+    DynamicAnswerForm = get_dynamic_answer_forms()
     if request.method == 'POST':
-        form = StudentRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Student created successfully.')
+        student_form = StudentRegistrationForm(request.POST)
+        answer_form = DynamicAnswerForm(request.POST)
+        if student_form.is_valid() and answer_form.is_valid():
+            student = student_form.save()
+            for i, question in enumerate(QUESTIONS):
+                answer_text = answer_form.cleaned_data.get(f'question_{i}')
+                if answer_text:
+                    Answer.objects.create(
+                        student=student,
+                        question_number=i,
+                        answer_text=answer_text
+                    )
             return redirect('student_list')
     else:
-        form = StudentRegistrationForm()
-    return render(request, 'yearbook/student_create.html', {'form': form})
+        student_form = StudentRegistrationForm()
+        answer_form = DynamicAnswerForm()
     
+    return render(request, 'yearbook/student_create.html', {
+        'student_form': student_form,
+        'answer_form': answer_form,
+    })
+
+
+
 @login_required
 def student_update(request, student_id):
     student = get_object_or_404(Student, student_id=student_id)
