da8c7211b8bcefa08d340b5ffc7e755151d4c1fd
neginkheirmand <neginkheirmand@gmail.com>
2024-07-06 06:36:38 +0330

    add requirements and dockerfile

--- my-yearbook/settings.py
+++ my-yearbook/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'my-yearbook.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {    
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
